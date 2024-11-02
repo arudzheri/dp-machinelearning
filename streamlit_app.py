cc402ea8db4ec08fed4b8c7c165305ea151615e3
w1996507 <143714867+arudzheri@users.noreply.github.com>
2024-11-02 14:26:19 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning- Chemical Molecule App')
 
 st.info('This is app that builds a machine learning model for chemical molecules!')
+
+molecule = st.text_input("Molecule", DEFAULT_MOL)
+smile_code = st_ketcher(molecule)
+st.markdown(f"Smile code: ``{smile_code}``")
