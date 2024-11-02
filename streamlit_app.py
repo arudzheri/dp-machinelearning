2a8c8d3f2e96b2ee1208d215313ce2b02d7edf8b
w1996507 <143714867+arudzheri@users.noreply.github.com>
2024-11-02 14:21:50 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🤖 Machine Learning- Chemical Molecule App')
 
-st.write('Hello world!')
+st.info('This is app that builds a machine learning model for chemical molecules!')
